a7eb6d31bd947ae96f7ed7e0e1ec67de762cced1
Marcos Pérez Ferro <djwedo@gmail.com>
2018-01-27 23:36:02 +0100

    Changes

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -7,9 +7,12 @@ public class CameraShake : MonoBehaviour
     // if null.
     public Transform camTransform;
 
+    // How long the object should shake for.
+    public float shakeDuration = 0f;
+
     // Amplitude of the shake. A larger value shakes the camera harder.
-    public float shakeAmount = 0.1f;
-    public float decreaseFactor = 0.2f;
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
 
     Vector3 originalPos;
 
@@ -28,9 +31,16 @@ public class CameraShake : MonoBehaviour
 
     void Update()
     {
-        
+        if (shakeDuration > 0)
+        {
             camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
-        
-       
+
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            camTransform.localPosition = originalPos;
+        }
     }
 }
