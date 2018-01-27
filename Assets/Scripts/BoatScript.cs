4d14546001fe9b2e3c7d2efbfd40c2f69aa8dcd0
Luis Sebastián Huerta <luissebastianhuerta@gmail.com>
2018-01-27 19:55:35 +0100

    Add some assets

---
+++ Assets/Scripts/BoatScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    
+
+    public float speed = 1.5f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "rock")
+        {
+
+            Destroy(this);
+        }
+    }
+}
