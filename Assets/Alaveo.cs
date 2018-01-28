8d1b31a53f19ad867ccdc8331f17299e67d57f22
Luis Sebastián Huerta <luissebastianhuerta@gmail.com>
2018-01-28 12:07:51 +0100

    Add alaveo script

---
+++ Assets/Alaveo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alaveo : MonoBehaviour {
+
+    public float duration = 1.0F;
+    public Light lt;
+    public GameObject plano;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+    // Update is called once per frame 
+    void Update()
+    {
+        plano.transform.Rotate(0, 0, Time.deltaTime * 3 * 45);
+    }
+}
